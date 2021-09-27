a19e6899e951fd06ea4ba5f200b0429436d27b79
Nemezis <maksimkov2000@mail.ru>
2021-09-27 12:47:18 +0300

    Созданы страницы из раздела "О нас"

---
+++ Controllers/AboutController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RestaurantBusiness.Controllers
+{
+    public class AboutController : Controller
+    {
+        public IActionResult Restaurants()
+        {
+            ViewBag.Title = "Рестораны";
+            return View();
+        }
+
+        public IActionResult Service()
+        {
+            ViewBag.Title = "Обслуживание";
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewBag.Title = "Контакты";
+            return View();
+        }
+
+        public IActionResult Awards()
+        {
+            ViewBag.Title = "Награды";
+            return View();
+        }
+
+        public IActionResult Staff()
+        {
+            ViewBag.Title = "Персонал";
+            return View();
+        }
+
+        public IActionResult Reviews()
+        {
+            ViewBag.Title = "Отзывы";
+            return View();
+        }
+    }
+}
--- Controllers/OrderTableController.cs
+++ Controllers/OrderTableController.cs
@@ -1,20 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace RestaurantBusiness.Controllers
 {
-    public class HomeController : Controller
+    public class OrderTableController : Controller
     {
-        public HomeController()
-        {
-
-        }
-
         public IActionResult Index()
         {
-            ViewBag.Title = "Главная страница";
             return View();
         }
-
-
     }
 }
