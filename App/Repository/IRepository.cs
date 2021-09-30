d7ed9c0d6785fba2a933b618d9803a88419bca58
Nemezis <maksimkov2000@mail.ru>
2021-09-30 20:52:10 +0300

    Создан репозиторий для взаимодействия с БД

---
+++ App/Repository/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestaurantBusiness.App.Repository
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        IQueryable<TEntity> GetAll();
+
+        Task<TEntity> GetByIdAsync(Guid id);
+
+        Task AddAsync(TEntity model);
+
+        Task UpdateAsync(TEntity model);
+
+        Task DeleteAsync(TEntity model);
+
+        Task SaveChangesAsync();
+    }
+}
